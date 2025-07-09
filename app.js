f7cb8e05590673d46f6e86c0d079501923e05155
unknown <muktinathrajbanshi786@gmail.com>
2025-07-09 18:00:40 +0545

    Rest API : Setup Routes & Controllers using Express Routers

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const products_routes = require("./routes/products");
+
 app.get("/", (req, res) => {
     res.send("Hi, I am live");
 });
+
+
+// middleware or to set router
+app.use("/api/products", products_routes);
 
 const start = async () => {
     try {
