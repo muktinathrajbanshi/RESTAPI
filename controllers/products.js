7ab1e66724ad5a0f7d6b472f4811bd4cccfaaf55
unknown <muktinathrajbanshi786@gmail.com>
2025-07-11 18:03:23 +0545

    Adding Pagination in Rest API using Node & Mongoose

--- controllers/products.js
+++ controllers/products.js
@@ -32,13 +32,26 @@ const getAllProducts = async (req, res) => {
         apiData = apiData.select(selectFix);
     }
 
+
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 3;
+
+    let skip = (page -1) * limit;
+
+    // page =2;
+    // limit = 3;
+    // skip = 1 * 3 = 3
+
+    apiData = apiData.skip(skip).limit(limit);
+
      console.log(queryObject);
 
     const myData = await apiData;
-    res.status(200).json({myData});
+    res.status(200).json({myData, nbHits: myData.length});
 };
 
 const getAllProductsTesting = async (req, res) => {
+    console.log(req.query);
     const myData = await Product.find(req.query).select("name company");
     res.status(200).json({myData});
 };
