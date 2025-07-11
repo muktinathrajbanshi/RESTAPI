d7ef212c8b99519ef5526faf0843341d5fc4287a
unknown <muktinathrajbanshi786@gmail.com>
2025-07-11 20:25:16 +0545

    finally this project is also completed

--- controllers/products.js
+++ controllers/products.js
@@ -34,7 +34,7 @@ const getAllProducts = async (req, res) => {
 
 
     let page = Number(req.query.page) || 1;
-    let limit = Number(req.query.limit) || 3;
+    let limit = Number(req.query.limit) || 10;
 
     let skip = (page -1) * limit;
 
@@ -46,8 +46,8 @@ const getAllProducts = async (req, res) => {
 
      console.log(queryObject);
 
-    const myData = await apiData;
-    res.status(200).json({myData, nbHits: myData.length});
+    const Products = await apiData;
+    res.status(200).json({Products, nbHits: Products.length});
 };
 
 const getAllProductsTesting = async (req, res) => {
